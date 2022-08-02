47f8c3b8d23cf8cc1ad3d790731f4553ea6b9f61
David Roundy <daveroundy@gmail.com>
2022-08-02 12:20:22 -0700

    when switching to a layer ensure we can draw immediately

--- src/main.rs
+++ src/main.rs
@@ -98,20 +98,28 @@ fn color_selector(color: &mut Color) -> bool {
 }
 
 impl Drawing {
-    fn layer_selector(&mut self, current_layer: &mut usize) -> bool {
+    fn layer_selector(&mut self) -> bool {
         const WIDTH: f32 = 50.0;
         const HEIGHT: f32 = 40.0;
+        fn outline(y: f32, selected: bool) {
+            draw_rectangle_lines(0.0, y, WIDTH, HEIGHT, 5.0, BLACK);
+            draw_rectangle_lines(
+                2.0,
+                y + 2.0,
+                WIDTH - 4.0,
+                HEIGHT - 4.0,
+                5.0,
+                if selected { WHITE } else { GRAY },
+            );
+        }
         for (i, l) in self.layers.iter().enumerate() {
             let y = i as f32 * HEIGHT;
             draw_rectangle(0.0, y, WIDTH, HEIGHT, l.color);
-            let color = if i == *current_layer { WHITE } else { GRAY };
-            draw_rectangle_lines(0.0, y, WIDTH, HEIGHT, 5.0, BLACK);
-            draw_rectangle_lines(2.0, y + 2.0, WIDTH - 4.0, HEIGHT - 4.0, 5.0, color);
+            outline(y, i == self.current);
         }
         let y = self.layers.len() as f32 * HEIGHT;
         draw_rectangle(0.0, y, WIDTH, HEIGHT, BLACK);
-        draw_rectangle_lines(0.0, y, WIDTH, HEIGHT, 5.0, BLACK);
-        draw_rectangle_lines(2.0, y + 2.0, WIDTH - 4.0, HEIGHT - 4.0, 5.0, GRAY);
+        outline(y, false);
         draw_line(
             WIDTH * 0.5,
             y + HEIGHT * 0.5 - 10.0,
@@ -128,10 +136,17 @@ impl Drawing {
             5.0,
             WHITE,
         );
+        let y = y + 2.0 * HEIGHT;
+        outline(y, self.tool == Tool::BigPen);
+        draw_circle(WIDTH * 0.5, y + HEIGHT * 0.5, 10.0, WHITE);
+        outline(y + HEIGHT, self.tool == Tool::LittlePen);
+        draw_circle(WIDTH * 0.5, y + HEIGHT * 1.5, 2.0, WHITE);
+        outline(y + 2.0 * HEIGHT, self.tool == Tool::Eraser);
+        draw_circle_lines(WIDTH * 0.5, y + HEIGHT * 2.5, 10.0, 2.0, WHITE);
         if is_mouse_button_down(MouseButton::Left) || is_mouse_button_pressed(MouseButton::Left) {
             let (x, y) = mouse_position();
             let y = (y / HEIGHT) as usize;
-            if x < WIDTH && y <= self.layers.len() {
+            if x < WIDTH {
                 if is_mouse_button_pressed(MouseButton::Left) {
                     if y == self.layers.len() {
                         self.layers.push(Layer {
@@ -147,9 +162,21 @@ impl Drawing {
                             ),
                             color: WHITE,
                         });
-                        *current_layer = self.layers.len() - 1;
-                    } else {
-                        *current_layer = y;
+                        self.current = self.layers.len() - 1;
+                        if self.tool == Tool::Eraser {
+                            self.tool = Tool::BigPen;
+                        }
+                    } else if y < self.layers.len() {
+                        self.current = y;
+                        if self.tool == Tool::Eraser {
+                            self.tool = Tool::BigPen;
+                        }
+                    } else if y == self.layers.len() + 2 {
+                        self.tool = Tool::BigPen;
+                    } else if y == self.layers.len() + 3 {
+                        self.tool = Tool::LittlePen;
+                    } else if y == self.layers.len() + 4 {
+                        self.tool = Tool::Eraser;
                     }
                 }
                 return true;
@@ -164,9 +191,18 @@ struct Layer {
     bitmap: Image,
 }
 
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+enum Tool {
+    Eraser,
+    BigPen,
+    LittlePen,
+}
+
 struct Drawing {
+    current: usize,
     height: u16,
     width: u16,
+    tool: Tool,
     layers: Vec<Layer>,
 }
 
@@ -175,9 +211,9 @@ async fn main() {
     let mut old_pos: Option<Vec2> = None;
     let image = Image::gen_image_color(2000, 1024, BLACK);
     let width = image.width as usize;
-    let radius = 5.0;
-    let mut current_layer = 0;
     let mut drawing = Drawing {
+        current: 0,
+        tool: Tool::BigPen,
         width: image.width,
         height: image.height,
         layers: vec![Layer {
@@ -194,12 +230,22 @@ async fn main() {
         for l in drawing.layers.iter() {
             draw_texture(Texture2D::from_image(&l.bitmap), 0.0, 0.0, l.color);
         }
-        let color_selected = color_selector(&mut drawing.layers[current_layer].color);
-        let layer_selected = drawing.layer_selector(&mut current_layer);
+        let color_selected = color_selector(&mut drawing.layers[drawing.current].color);
+        let layer_selected = drawing.layer_selector();
         if !root_ui().is_mouse_captured() && !color_selected && !layer_selected {
             if is_mouse_button_down(MouseButton::Left) {
                 let pos = mouse_position();
                 let pos = Vec2::new(pos.0, pos.1);
+                let radius = if drawing.tool == Tool::LittlePen {
+                    2.0
+                } else {
+                    10.0
+                };
+                let drawn = if drawing.tool == Tool::Eraser {
+                    [0; 4]
+                } else {
+                    [255; 4]
+                };
                 if let Some(old) = old_pos {
                     let parallel = (old - pos).normalize();
                     let orthog = Vec2::new(parallel.y, -parallel.x);
@@ -224,7 +270,7 @@ async fn main() {
                         std::cmp::min(pos.y as usize, old.y as usize),
                     ) - radius as usize;
                     let y_stop = std::cmp::min(
-                        drawing.layers[current_layer].bitmap.height as usize
+                        drawing.layers[drawing.current].bitmap.height as usize
                             - 1
                             - (radius as usize),
                         std::cmp::max(pos.y as usize, old.y as usize),
@@ -237,8 +283,8 @@ async fn main() {
                                 && here.dot(parallel) > par_start
                                 && here.dot(parallel) < par_stop
                             {
-                                drawing.layers[current_layer].bitmap.get_image_data_mut()
-                                    [x + y * width] = [255, 255, 255, 255];
+                                drawing.layers[drawing.current].bitmap.get_image_data_mut()
+                                    [x + y * width] = drawn;
                             }
                         }
                     }
@@ -257,8 +303,8 @@ async fn main() {
                     for y in y_start..y_stop {
                         if (x as f32 - pos.x).powi(2) + (y as f32 - pos.y).powi(2) < radius.powi(2)
                         {
-                            drawing.layers[current_layer].bitmap.get_image_data_mut()
-                                [x + y * width] = [255, 255, 255, 255];
+                            drawing.layers[drawing.current].bitmap.get_image_data_mut()
+                                [x + y * width] = drawn;
                         }
                         old_pos = Some(pos);
                     }
